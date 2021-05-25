dc42f8ddfff9441aba794e83b6eb239b61e934f8
Daniel Cuevas <daniel_cuevas_hernandez@outlook.com>
2021-05-25 09:49:47 -0500

    Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../assets/styles/components/Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <button className="footer__button btn">Rules</button>
+    </footer>
+  );
+};
+
+export default Footer;
