3e14b845cbdc3ee3a808f9768ea42b39b2125ce6
Daniel Cuevas <daniel_cuevas_hernandez@outlook.com>
2021-05-26 12:02:21 -0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Modal from "./components/Modal";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainStart from "./components/MainStart";
+
 import "./assets/styles/Reset.scss";
 import "./assets/styles/App.scss";
+import MainPlaying from "./components/MainPlaying";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <MainStart />
+      {/* <MainPlaying /> */}
       <Modal />
+      <Footer />
     </div>
   );
 }
