ecc6e1d5d80eb58bfe242f8a87faafba4981f59e
Daniel Cuevas <daniel_cuevas_hernandez@outlook.com>
2021-05-27 13:10:51 -0500

    Add score

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,15 @@ import logo from "../assets/images/logo.svg";
 
 import "../assets/styles/components/Header.scss";
 
-const Header = () => {
+const Header = (props) => {
+  const { score } = props;
   return (
     <header>
       <div className="header">
         <img className="header__logo" src={logo} alt="logo" />
         <div className="header__score">
           <p className="header__score__title">score</p>
-          <p className="header__score__score">12</p>
+          <p className="header__score__score">{score}</p>
         </div>
       </div>
     </header>
